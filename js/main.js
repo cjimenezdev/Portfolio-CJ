42806b21c4030431346c007b7a339d8a4d8a4d22
Carlos Jiménez <carlosjimenez32077@gmail.com>
2022-05-09 22:30:19 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,3 +20,49 @@ init();
 $(".sidebar__toggle").click(function() {
     $(".sidebar__menu").toggleClass("show").siblings().removeClass("show");
 });
+
+function enviarCorreo(){
+	$(".alerta_correo").css('display', 'none');
+	$(".alerta_mensaje").css('display', 'none');
+	$('.alerta_enviado').css('display','none');
+	$('.alerta_error').css('display','none');
+	
+	var correo = $("#correo").val();
+	var mensaje = $("#mensaje").val();
+	var nombre = $("#nombre").val();
+    var asunto = $("#asunto").val();
+	var valido = 1;
+	var validacion_correo = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+	
+	
+	if(!validacion_correo.test(correo)){
+		$(".alerta_correo").css('display', 'block');
+		valido = 0;
+	}
+	if(mensaje.length <= 5 ){
+		$(".alerta_mensaje").css('display', 'block');
+		valido = 0;
+	}
+
+	if (valido == 1) {
+		var datos = 'correo=' + correo + '&mensaje=' + mensaje + '&nombre=' + nombre  + '&asunto=' + asunto ;
+        $.ajax({
+			type: "POST",
+			url: "contact.php",
+			data: datos,
+			success: function(res) {
+				if (parseInt(res) == 1) {
+				    let formulario = document.getElementById('formulario');
+                    formulario.reset();
+                    
+				    $('.alerta_enviado').css('display','block');
+				}else{
+					$('.alerta_error').css('display','block');
+				}
+			},
+			error: function(res) {
+				$('.alerta_error').css('display','block');
+			}
+		});
+	}
+}
